fae2695a06eba56b8fc366b718b09735c2d8a727
SamKirkiles <sam.kirkiles@gmail.com>
2018-05-22 16:49:56 -0400

    trying something with batching

--- main.py
+++ main.py
@@ -6,28 +6,9 @@ import matplotlib.pyplot as plt
 def main():
 	print("running main")
 
-	#vgg = VGG()
+	vgg = VGG()	
+	vgg.train(generator,False)
 
-	# Load CIFAR-100 dataset
-	loader = Loader()
-
-	iterator = loader.get_dataset()
-
-	print("stuck here")
-	batch = (iterator.get_next())
-
-	with tf.Session() as sess:
-		x,y = sess.run(batch) 
-		print(x.shape)
-		print(y.shape)
-
-	
-	#vgg.train(generator,False)
-
-	"""
-	test_y_one_hot = np.eye(test_y.shape[0])[test_y]
-	train_y_one_hot = np.eye(train_y.shape[0])[np.ravel(train_y)]
-	"""
 
 if __name__ == "__main__":
 	main()
