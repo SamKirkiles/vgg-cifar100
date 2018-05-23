145539399ad063eecd332773baf56555369cda44
SamKirkiles <sam.kirkiles@gmail.com>
2018-05-22 21:08:54 -0400

    fixed main file

--- main.py
+++ main.py
@@ -6,20 +6,8 @@ from data_loader import Loader
 def main():
 	print("running main")
 
-	#vgg = VGG()	
-	#vgg.train(False)
-	loader = Loader()
-
-	with tf.Session() as sess:
-
-		iterator = loader.get_dataset()
-		element = iterator.get_next()
-
-		x,y = sess.run(element)
-
-		#print(x)
-		#plt.imshow(x[0])
-		#plt.show()
+	vgg = VGG()	
+	vgg.train(False)
 
 
 
