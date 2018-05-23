e74c2eb0158c54f722c7dcf14665e07c2bb0a2ee
SamKirkiles <sam.kirkiles@gmail.com>
2018-05-22 22:19:08 -0400

    fixed default values of validation

--- vgg.py
+++ vgg.py
@@ -126,7 +126,7 @@ class VGG:
 				counter = 0
 
 				train_loader = Loader()
-				val_x,val_y = train_loader.get_dataset(train=False).get_next()
+				val_x,val_y = sess.run(train_loader.get_dataset(train=False).get_next())
 
 				while True:
 
