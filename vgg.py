61015803865ff4211152aa8af13d641bb6acf440
SamKirkiles <sam.kirkiles@gmail.com>
2018-05-23 16:09:38 -0400

    undid last action

--- vgg.py
+++ vgg.py
@@ -101,7 +101,7 @@ class VGG:
 
 				# Predict
 				scaled_logits = -tf.log(dense16)
-				prediction = tf.argmax(tf.nn.softmax(scaled_logits),axis=0)
+				prediction = tf.argmax(tf.nn.softmax(scaled_logits),axis=1)
 
 				equality = tf.equal(prediction, y)
 				accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
